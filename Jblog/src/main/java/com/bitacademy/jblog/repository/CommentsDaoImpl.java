2f8a69e3bade24166660ae7da1a671dadda4d500
김상민 <wrt123123123@gmail.com>
2021-04-26 17:32:59 +0900

    중간 commit

--- Jblog/src/main/java/com/bitacademy/jblog/repository/CommentsDaoImpl.java
+++ Jblog/src/main/java/com/bitacademy/jblog/repository/CommentsDaoImpl.java
@@ -15,17 +15,17 @@ public class CommentsDaoImpl implements CommentsDao {
 	
 	@Override
 	public int insertComments(CommentsVo vo) {
-		int insertedCount = sqlSession.insert("blog.insertComment", vo);
+		int insertedCount = sqlSession.insert("blog.insertComments", vo);
 		return insertedCount;
 	}
 	@Override
 	public int deleteComments(Long no) {
-		int deletedCount = sqlSession.delete("blog.deleteComment", no);
+		int deletedCount = sqlSession.delete("blog.deleteComments", no);
 		return deletedCount;
 	}
 	@Override
 	public List<CommentsVo> getComments(Long no) {
-		List<CommentsVo> list = sqlSession.selectList("blog.getComment", no);
+		List<CommentsVo> list = sqlSession.selectList("blog.getComments", no);
 		return list;
 	}
 
